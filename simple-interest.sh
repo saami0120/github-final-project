8e12f0c62454941f74ec39f3c20b7d55b20700b0
saami0120 <167550093+saami0120@users.noreply.github.com>
2024-09-11 18:51:51 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
+   # Author: Upkar Lidder (IBM?
    # Additional Authors:
-   # <your GitHub username>
+   #saami0120
 
    # Input:
    # p, principal amount
